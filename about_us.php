676ed5afb22e328bc0f2e3578814b328e12c1385
Rutuja Vijay Shinde <95953805+rutujashinde1104@users.noreply.github.com>
2023-09-09 10:37:45 +0530

    Update about_us.php

--- about_us.php
+++ about_us.php
@@ -5,19 +5,9 @@
 <tr>
 	<td><img src='images/1.jpg' width='200px' height='200px'/>
 	<td>
-	<b>Name:</b>Arti<br>
-	<b>Roll No:</b>2925<br>
-	<b>Class:</b>TY BCS<br>
-	</td>
-</tr>
-</table><hr style='width:50%;'>
-<table align='center' style='background:linen;padding:20px;'>
-<tr style='border:1px solid red;'>
-	<td><img src='images/2.jpg' width='200px' height='200px'/>
-	<td>
-	<b>Name:</b>Madhuri<br>
-	<b>Roll No:</b>2940<br>
-	<b>Class:</b>TY BCS<br>
+	<b>Name:</b>Rutuja Shinde<br>
+	<b>Roll No:</b>60<br>
+	<b>Class:</b>mca<br>
 	</td>
 </tr>
 </table>
